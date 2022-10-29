b5922c9cda17d0ef4a7aebbf3335e0260c4294f1
G-Kiruthika <gkiruthika15@gmail.com>
2022-10-29 22:26:55 +0530

    with map

--- src/screen/contact/Contact.jsx
+++ src/screen/contact/Contact.jsx
@@ -1,21 +1,43 @@
 import React, { Component } from 'react'
-
+import Navbar from '../../component/Navbar/Navbar'
+import Footer from '../../component/Footer/Footer'
+import './contact.css'
 export class Contact extends Component {
   render() {
     return (
       <div>
-        <div className='row'>
-            <div className='col-md-6'>
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div> 
+        <Navbar/>
+        <div className='header'>
+           <h3 className='company-name'>ABC Company</h3>
+        </div>
+        <div className='bg-header'>
+          <div className='header-text'>
+            <h4>QWERTYUIOPASDFGHJKL</h4>
+            <h5>about</h5>
+          </div>
+        </div>
+        <div className='details'>
+          <form className=' container shadow'>
+            <div className='row'>
+              <div className='col'>
+            <label htmlFor="firstname">First Name</label>
+            <input type="text" id='firstname' className='form-control' />
             </div>
+            <div className='col'>
+              <label htmlFor="lastname" className='form-label'>Last Name</label>
+              <input type="text" id='lastname' className='form-control' />
+            </div>
+            </div>
+            <label htmlFor="phone" className='form-label'>Phone number</label>
+            <input type="text" id="phone" className='form-control'/>
+            <label htmlFor="email" className='form-label' >Email</label>
+            <input type="text" id='email' className='form-control' /> <br />
+            <label htmlFor="prblm" className='form-label'>Problem</label>
+            <input type="text" name="prblm" id="prblm" className='form-control'/> <br />
+            <label htmlFor="location" className='form-label'>Location</label>
+            <input type="text" id='location' className='form-control' /> <br />
+            <button classname="btn" type="submit">Submit</button>
+          </form>
         </div>
       </div>
     )
--- src/screen/map/CustomMap.jsx
+++ src/screen/map/CustomMap.jsx
@@ -3,53 +3,83 @@ import axios from 'axios'
 import './custommap.css'
 import Navbar from '../../component/Navbar/Navbar'
 import Footer from '../../component/Footer/Footer'
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia2lydXRoaWthZyIsImEiOiJjbDkydzlnMmQxMHhmM29xaDNscDI3NzYwIn0.7nG-L0LTVEcxQPUfQaze3g';
+// import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+ import Map, { Marker, NavigationControl,GeolocateControl, FullscreenControl } from "react-map-gl"; 
+ import 'mapbox-gl/dist/mapbox-gl.css';
+ import pin from '../../asserts/images/marker.png'
 function CustomMap() {
-  // setCompany(JSON.parse(localStorage.getItem("company")))
+  // // setCompany(JSON.parse(localStorage.getItem("company")))
+  
+  // const mapContainer = useRef(null);
+  // const map = useRef(null);
+  // const [lng, setLng] = useState(77.7172);
+  // const [lat, setLat] = useState(11.3410);
+  // const [zoom, setZoom] = useState(13);
+  // const [company,setCompany]= useState(JSON.parse(localStorage.getItem("company")))
 
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(77.7172);
-  const [lat, setLat] = useState(11.3410);
-  const [zoom, setZoom] = useState(13);
-  const [company,setCompany]= useState(JSON.parse(localStorage.getItem("company")))
-
-  useEffect(async() => {
+  // useEffect(async() => {
     
-    console.log(company);
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude,position.coords.longitude);
-    })
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
-  });
+  //   console.log(company);
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     console.log(position.coords.latitude,position.coords.longitude);
+  //   })
+  //   if (map.current) return; // initialize map only once
+  //   map.current = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/mapbox/streets-v11',
+  //     center: [lng, lat],
+  //     zoom: zoom
+  //   });
+  // });
 
-  const fetch =async()=>{
-    await axios({
-      method: 'get',
-      url: 'http://localhost:4000/user/',
-    }).then(
-      (res)=>{
-        console.log(res);
-        setCompany(res)
-    }
-    ).catch((err)=>console.log(err))
-  }
-
-
+  // const fetch =async()=>{
+  //   await axios({
+  //     method: 'get',
+  //     url: 'http://localhost:4000/user/',
+  //   }).then(
+  //     (res)=>{
+  //       console.log(res);
+  //       setCompany(res)
+  //   }
+  //   ).catch((err)=>console.log(err))
+  // }
+  const [lng, setLng] = useState(54.37585762735543);
+  const [lat, setLat] = useState(24.45677614934833);
+  
   return (
+    
     <div>
-      <Navbar/>
+    <Map mapboxAccessToken = 'pk.eyJ1Ijoia2lydXRoaWthZyIsImEiOiJjbDkydzlnMmQxMHhmM29xaDNscDI3NzYwIn0.7nG-L0LTVEcxQPUfQaze3g'
+        style={{width: "99vw",
+                 height:"99vh",
+                borderRadius: "15px",
+                border: '2px solid red '}}
+        initialViewState={{
+          longitude:-100,
+          latitude:40,
+          zoom:3.5
+        }}
+      mapStyle="mapbox://styles/mapbox/streets-v9">
+        {/* <Marker
+          Longitude={-100}
+          Latitude={40}
+        /> */}
+              {company.map((ele,index)=>{
+        return(
+          <Marker longitude={-100} latitude={40} anchor="bottom" >
+          <img src={pin} />
+    </Marker>
+        )
+      })}
+     
+      <NavigationControl position="top-left"/>
+      <FullscreenControl />
+      <GeolocateControl />
+      </Map>
+      {/* <Navbar/>
       <div ref={mapContainer} className="map-container" />
       <div className='container mt-5'>
-      <table className='table table-striped table-hover'>
+      <table className='table table-striped table-hover table-dark'>
       <thead>
         <tr>
           <td>Company</td>
@@ -60,22 +90,22 @@ function CustomMap() {
         </tr>
       </thead>
       <tbody>
-      {company.map((ele,index)=>{
-        return(
-          <tr key={index}>
-            <td>{ele.company}</td>
-            <td>{ele.email}</td>
-            <td>{ele.year}</td>
-            <td>{ele.lat}</td>
-            <td>{ele.long}</td>
-          </tr>
-        )
-      })}
+      // {company.map((ele,index)=>{
+      //   return(
+      //     <tr key={index}>
+      //       <td>{ele.company}</td>
+      //       <td>{ele.email}</td>
+      //       <td>{ele.year}</td>
+      //       <td>{ele.lat}</td>
+      //       <td>{ele.long}</td>
+      //     </tr>
+      //   )
+      // })}
         
       </tbody>
       </table>
       </div>
-      <Footer/>
+      <Footer/> */}
     </div>
   );
 }
